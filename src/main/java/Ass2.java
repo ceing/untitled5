9272c65e1bd01d7a523a61aee7fb9e77c8dab6d7
ceing <ai39@163.com>
2020-05-15 16:57:30 +0800

    update 2 3 5

--- src/main/java/Ass2.java
+++ src/main/java/Ass2.java
@@ -1,5 +1,6 @@
 public class Ass2 {
     public static void main(String[] args) {
+        int a = 0;
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
             try {
--- src/main/java/Axx3.java
+++ src/main/java/Axx3.java
@@ -1,5 +1,7 @@
 public class Axx3 {
     public static void main(String[] args) {
         int a;
+        int b;
+
     }
 }
